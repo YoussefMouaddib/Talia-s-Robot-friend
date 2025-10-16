c4f1133a916d2a8af89e10e249b5ca94d970fc7b
YoussefMouaddib <76605804+YoussefMouaddib@users.noreply.github.com>
2025-10-16 17:07:11 -0500

    Create config.py

---
+++ pi/src/config.py
@@ -0,0 +1,16 @@
+# config.py
+
+# ASR
+ASR_MODEL_PATH = "../models/whisper.cpp/"
+
+# TTS
+TTS_VOICE = "child"  # Coqui TTS preset
+TTS_OUTPUT_DIR = "../audio_output/"
+
+# TCP Client
+TCP_HOST = "LAPTOP_IP_ADDRESS"  # replace with Laptop's IP
+TCP_PORT = 5555
+
+# Audio
+MIC_DEVICE = "default"
+SPEAKER_DEVICE = "default"
